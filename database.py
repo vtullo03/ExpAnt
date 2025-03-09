40f522c4440e3162714b0b2b0da9e588cbfb99d4
vtullo03 <vitotullo430@gmail.com>
2025-03-08 23:24:17 -0500

    user registration

--- database.py
+++ database.py
@@ -1,20 +1,21 @@
 ﻿from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.getenv("\ufeffDATABASE_URL")
+print(os.environ)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
 
 def get_db():
-    db = SessionLocal()
+    db = Session()
     try:
         yield db
     finally:
